5c984b5feba7daab0cd961073c61a5d0ffff97fe
dilankaplan <45143995+dilankaplan@users.noreply.github.com>
2021-07-19 07:31:07 +0300

    .

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -7,7 +7,7 @@ using System.Text;
 
 namespace Business.Concrete
 {
-    public class ProductManager : IProductService
+    public class ProductManager :IProductService
     {
         IProductDal _productDal;
 
@@ -20,6 +20,17 @@ namespace Business.Concrete
         {
             //Business Code
             return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice>= min && p.UnitPrice <= max);
         }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,13 +4,14 @@ using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
-{
+{//SOLID
+    //Open Close Principle
     class Program
     {
         static void Main(string[] args)
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetAll()) 
+            foreach (var product in productManager.GetAllByCategoryId(2)) 
             {
                 Console.WriteLine(product.ProductName);
             }
