f80cca66482d0e61f6e4eb6699c48351d48aba19
dilankaplan <45143995+dilankaplan@users.noreply.github.com>
2021-07-19 07:14:15 +0300

    Entity Framework

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{    //Generic constraint
+    //IEntity olabililr ya da IEntity implemente eden bir nesne olabilir
+    //new: newlenebilmeli
+    public interface IEntityRepository<T > where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        List<T> GetAllByCategory(int categoryId);
+    }
+}
