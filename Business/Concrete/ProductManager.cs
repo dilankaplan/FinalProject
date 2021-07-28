db0f6fb9476f335ee7269bf8e40ac78a4229dc38
dilankaplan <45143995+dilankaplan@users.noreply.github.com>
2021-07-28 22:37:20 +0300

    .

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -27,6 +27,7 @@ namespace Business.Concrete
             _productDal = productDal;
             _categoryService = categoryService;
         }
+        
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
