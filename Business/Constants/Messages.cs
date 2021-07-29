1653fa649d3a36bae1937d09251e3f8d0c4a9d86
dilankaplan <45143995+dilankaplan@users.noreply.github.com>
2021-07-29 22:08:21 +0300

    JWT

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -13,5 +15,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError="Bir kategoriden en fazla 10 ürün olabilir.";
         public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var.";
         public static string CategoryLimitExceded="Kategori limit aşıldığı için yeni ürün eklenemiyor.";
+        public static string AuthorizationDenied ="Yetkiniz yok.";
+        public static string UserRegistered ="Kullanıcı kayıtlı";
+        public static string UserNotFound ="Kullanıcı bulunamadı";
+        internal static string PasswordError="Şifre hatalı";
+        internal static string SuccessfulLogin="Giriş başarılı.";
+        internal static string UserAlreadyExists="Kullanıcı mevcut.";
+        internal static string AccessTokenCreated="Access Token oluşturuldu.";
     }
 }
--- Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
+++ Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
@@ -7,7 +7,7 @@ namespace Core.Utilities.Security.Encryption
 {
     public class SigningCredentialsHelper
     {
-        public static SigningCredentials CreateSigningCredentiaks(SecurityKey security)
+        public static SigningCredentials CreateSigningCredentials(SecurityKey security)
         {
             return new SigningCredentials(security, SecurityAlgorithms.HmacSha512Signature);
         }
